6e53244ec0b39be893431bed75d15bb1356626e7
KINGG_trollX <72470675+yuna478@users.noreply.github.com>
2022-01-24 22:58:26 +0330

    git 1.2.9

--- code/SRF05.ino
+++ code/SRF05.ino
@@ -1,5 +1,5 @@
-const unsigned int TRIG_PIN=13;
-const unsigned int ECHO_PIN=12;
+const unsigned int TRIG_PIN=6;
+const unsigned int ECHO_PIN=5;
 const unsigned int BAUD_RATE=9600;
 
 void setup() {
